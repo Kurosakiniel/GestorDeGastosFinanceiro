4e5c6ed9584335d57a302890a70be26e4f1dffa5
Kurosakiniel <ichigoniel@gmail.com>
2026-03-24 19:44:02 -0300

    Logica dos Botões adicionados

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // elementos para o  modal
 const modal = document.getElementById("modal");
-const btnMais = document.querySelectorAll(".tab-btn")[1]; // botão +
+const btnMais = document.getElementById("btnMais"); // botão +
 const btnCancelar = document.getElementById("btnCancelar");
 const btnAddGasto = document.getElementById("btnAddGasto");
 // Para o tabs
 let gastos = [];
+let categoriaAtiva = "Geral";
 
+const tabGeral = document.getElementById("tabGeral");
 const inputGasto = document.getElementById("inputGasto");
 const btnConcluir = document.getElementById("btnConcluir");
 const tabsContainer = document.querySelector(".tabs");
@@ -31,12 +33,18 @@ btnAddGasto.addEventListener("click", abrirModal);
 
 // Fim da logica do modal
 
+
+// Logica de tabs
 // Tabs criadas 
 function criarTab(nome) {
   const novaTab = document.createElement("button");
   
   novaTab.classList.add("tab-btn");
   novaTab.textContent = nome;
+
+  novaTab.addEventListener("click", () => {
+    ativarTab(nome, novaTab);
+  });
 
   // insere antes do botão "+"
   tabsContainer.insertBefore(novaTab, btnMais);
@@ -61,4 +69,27 @@ btnConcluir.addEventListener("click", () => {
 
   // fecha modal
   modal.style.display = "none";
+});
+
+// Logica para selecionar Tabs
+
+function ativarTab(nome, elemento) {
+
+  // salva qual está ativa
+  categoriaAtiva = nome;
+
+  // remove active de todas
+  const tabs = document.querySelectorAll(".tab-btn");
+  tabs.forEach(tab => tab.classList.remove("active"));
+
+  // adiciona active na clicada
+  elemento.classList.add("active");
+
+  console.log("Categoria ativa:", categoriaAtiva);
+}
+
+// Logica ta tab Geral
+
+tabGeral.addEventListener("click", () => {
+  ativarTab("Geral", tabGeral);
 });
